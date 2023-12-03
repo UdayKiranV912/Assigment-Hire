bd04ffd9714fa5e58d6ab99d08774b335c93b162
Uday KIran V <85879936+UdayKiranV912@users.noreply.github.com>
2023-12-03 23:04:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add your JavaScript code here
+function submitLoginForm() {
+  // Implement login logic
+  console.log('Login button clicked');
+}
+
+function submitSignupForm() {
+  // Implement signup logic
+  console.log('Signup button clicked');
+}
